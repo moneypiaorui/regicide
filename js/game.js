71a686d094b15fe05eed59b5fa58d46e4ff6920e
moneypiaorui <2451879312@qq.com>
2023-11-26 13:02:46 +0800

    f

---
+++ js/game.js
@@ -0,0 +1,40 @@
+originalCards = 6;//初始手牌
+cardsLimit = 8;//单人
+
+playButton = document.getElementById("play");
+payButton = document.getElementById("pay");
+skipButton = document.getElementById("skip");
+
+bossPile = new CardPile([], "bossPile", 1, 0);
+handPile = new CardPile([], "handPile", 1, 0);
+handPile.clickAllow = 1;
+wastePile = new CardPile([], "wastePile", 1, 1);
+drawPile = new CardPile([], "drawPile", 1, 1);
+
+for (let i = 1; i <= 4; i++) {
+    for (let j = 1; j <= 10; j++) {
+        drawPile.push([new PokerCard(j, i)]);
+    }
+}
+drawPile.shuffle();
+handPile.push(drawPile.cardList.splice(0, originalCards));
+
+// boss牌堆洗牌
+for (let j = 13; j >= 11; j--) {
+    temPile = new CardPile([], "wastePile", 1, 1);
+    for (let i = 1; i <= 4; i++) {
+        temPile.push([new PokerCard(j, i)]);
+    }
+    temPile.shuffle();
+    bossPile.push(temPile.cardList);
+}
+selectBoss();
+refresh();
+
+// 绑定按钮点击事件
+playButton.addEventListener("click", play);
+payButton.addEventListener("click", pay);
+payButton.classList.add("hide");
+skipButton.addEventListener("click", skip);
+
+
